608418050eace4efc76f464dc734846765d535d8
ruchit31 <kadakiaruchit@gmail.com>
2016-11-29 22:02:45 -0800

    Testing 1st Commit

--- src/Hello.java
+++ src/Hello.java
@@ -15,6 +15,8 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("Hello Pratek");
+		System.out.println("Hello CMPE 275");
+		
 	}
 
 }
