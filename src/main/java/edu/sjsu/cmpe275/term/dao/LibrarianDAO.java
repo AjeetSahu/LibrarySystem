f22c93330240d11f7793f95f9cd55efef842d259
AjeetSahu <ajeetkumar.sahu@sjsu.edu>
2016-12-04 01:40:13 -0800

    Added Librarian DAO and Services

---
+++ src/main/java/edu/sjsu/cmpe275/term/dao/LibrarianDAO.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.sjsu.cmpe275.term.dao;
+
+import edu.sjsu.cmpe275.term.model.Librarian;
+
+/**
+ * @author Ajeet
+ *
+ */
+public interface LibrarianDAO {
+	
+	public Librarian saveNewLibrarian(Librarian librarian);
+
+}
