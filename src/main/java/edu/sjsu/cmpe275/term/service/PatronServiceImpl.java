1b4e0d7e2a0605fdfa653a618e4426ad27bf0717
ruchit31 <kadakiaruchit@gmail.com>
2016-12-04 02:30:59 -0800

    added Patron DAO and Services

---
+++ src/main/java/edu/sjsu/cmpe275/term/service/PatronServiceImpl.java
@@ -0,0 +1,22 @@
+package edu.sjsu.cmpe275.term.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.sjsu.cmpe275.term.dao.PatronDAO;
+import edu.sjsu.cmpe275.term.model.Patron;
+
+public class PatronServiceImpl implements PatronService {
+		
+		@Autowired
+		private PatronDAO patronDAO;
+		
+		public void setPatronDAO(PatronDAO patronDAO) {
+			this.patronDAO = patronDAO;
+		}
+	
+		@Override
+		public Patron saveNewPatron(Patron patron) {
+			return patronDAO.saveNewPatron(patron);
+			
+		}
+}
